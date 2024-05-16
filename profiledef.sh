b3f7771f42bca7712a504f2d2b54618605188ea9
chun-awa <1951474558@qq.com>
2024-05-16 20:47:23 +0800

    update sudoers.d permissions

--- profiledef.sh
+++ profiledef.sh
@@ -18,6 +18,7 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/sudoers.d"]="0:0:750"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
