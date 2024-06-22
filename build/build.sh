842a32386f5f8ec781f5ab91fd5c6f290eca0beb
chun-awa <1951474558@qq.com>
2024-06-22 17:03:14 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
-cd /archiso
+cd /lingmo-archiso-build/archiso
 make PREFIX=/usr install
-cd /lingmo-archiso-build
-ls /usr/local/bin
-ls /usr/bin
-echo $PATH
-whereis mkarchiso
 mkarchiso -v . -o /out
