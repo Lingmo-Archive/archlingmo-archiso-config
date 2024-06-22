53c705d5108702b50ba6a9b481fa5a2b3a559521
chun-awa <1951474558@qq.com>
2024-06-22 22:07:27 +0800

    debug artifacts upload

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 set -e
 
+cat<<EOF
+
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                   O\ = /O
+               ____/---'\____
+             .   ' \\| |// .
+              / \\||| : |||// \
+            / _||||| -:- |||||- \
+              | | \\\ - /// | |
+            | \_| ''\---/'' | |
+             \ .-\__ - ___/-. /
+          ___. .' /--.--\ . . __
+       ."" '< .___\_<|>_/___.' >'"".
+      | | : - \.;\ _ /;./ -  : | |
+        \ \ -. \_ __\ /__ _/ .- / /
+======-.____-.___\_____/___.-____.-'======
+                   =---='
+.............................................
+         佛祖保佑             永无BUG
+EOF
+
 mkdir /grub-efi
 cd /grub-efi
 wget https://dl-cdn.alpinelinux.org/alpine/edge/main/x86/APKINDEX.tar.gz
@@ -19,3 +43,4 @@ cd /archiso
 make PREFIX=/usr install
 cd /lingmo-archiso-build
 mkarchiso -v . -o /out
+ls /out
