f503cd6bedae9415fefbf018f6f3a1f9f7a0e9c5
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-07-19 16:44:59 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,28 +1,10 @@
 #!/bin/sh
 set -e
 
-cat<<EOF
-
-                   _ooOoo_
-                  o8888888o
-                  88" . "88
-                  (| -_- |)
-                   O\ = /O
-               ____/---'\____
-             .   ' \\| |// .
-              / \\||| : |||// \
-            / _||||| -:- |||||- \
-              | | \\\ - /// | |
-            | \_| ''\---/'' | |
-             \ .-\__ - ___/-. /
-          ___. .' /--.--\ . . __
-       ."" '< .___\_<|>_/___.' >'"".
-      | | : - \.;\ _ /;./ -  : | |
-        \ \ -. \_ __\ /__ _/ .- / /
-======-.____-.___\_____/___.-____.-'======
-                   =---='
-.............................................
-         佛祖保佑             永无BUG
+cat>/usr/share/grub/sbat.csv<<EOF
+sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md
+grub,3,Free Software Foundation,grub,2:2.12-2,https//www.gnu.org/software/grub/
+grub.arch,1,Arch Linux,grub,2:2.12-2,https://archlinux.org/packages/core/x86_64/grub/
 EOF
 
 cd /archiso
