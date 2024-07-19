76ce34beb1dfa279133d00e8f2650792e491de55
Canadew <110163919+chun-awa@users.noreply.github.com>
2024-07-19 19:42:24 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 set -e
-
-cat>/usr/share/grub/sbat.csv<<EOF
-sbat,1,SBAT Version,sbat,1,https://github.com/rhboot/shim/blob/main/SBAT.md
-grub,3,Free Software Foundation,grub,2:2.12-2,https//www.gnu.org/software/grub/
-grub.arch,1,Arch Linux,grub,2:2.12-2,https://archlinux.org/packages/core/x86_64/grub/
-EOF
-
-cd /archiso
-make PREFIX=/usr install
-cd /lingmo-archiso-build
-mkarchiso -v . -o /out
+mkarchiso -v /config -o /out
